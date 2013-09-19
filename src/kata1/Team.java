f6328cfe232c513f7f2dfc53b696600cf341c7fa
yeralmu <yeralmu@yeralmu-PC>
2013-09-19 18:53:40 +0100

    primer kata1

---
+++ src/kata1/Team.java
@@ -0,0 +1,22 @@
+
+package kata1;
+
+
+public class Team {
+
+    private String team;
+
+    public Team(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    
+    
+}
